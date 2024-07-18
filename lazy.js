22474a4aaa0766c6920e29a54d2b5dc6cda3660b
wildanproject1 <159136411+wildanproject1@users.noreply.github.com>
2024-07-19 00:26:57 +0700

    lazy.js

--- lazy.js
+++ lazy.js
@@ -29,3 +29,36 @@ function loadScript(src, callback) {
   script.onload = callback;
   document.head.appendChild(script);
 }
+
+
+/*==================== Lazy load images ====================*/
+// Lazy load images
+document.addEventListener("DOMContentLoaded", function () {
+  const lazyImages = document.querySelectorAll("img.lazy");
+  const lazyImageObserver = new IntersectionObserver(function (
+    entries,
+    observer
+  ) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        const lazyImage = entry.target;
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.classList.remove("lazy");
+        lazyImageObserver.unobserve(lazyImage);
+      }
+    });
+  });
+
+  lazyImages.forEach(function (lazyImage) {
+    lazyImageObserver.observe(lazyImage);
+  });
+});
+
+// Asynchronously load non-critical scripts
+function loadScript(src, callback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.async = true;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
